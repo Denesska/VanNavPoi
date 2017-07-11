a41fb06fbbe163ac63f513cc8630531e2fe0cc11
Denis Gandzii <denesska@gmail.com>
2017-07-11 18:14:56 +0300

    change drag and drop algorithm, added new script and page.

--- scripts/div_drag.js
+++ scripts/div_drag.js
@@ -1,14 +1,45 @@
 /**
- * Created by d.gandzii on 7/5/2017.
+ * Created by d.gandzii on 7/11/2017.
  */
+/*$(window).ready(function () {
+ var canvas = document.getElementById('canvasConstructor');
+ var can = $('#canvasConstructor')[0];
+ var ctx = can.getContext('2d');
+ ctx.fillStyle = 'Red';
+ ctx.fillRect(10, 10, 300, 100);
+ ctx.fillStyle = 'Blue';
+ ctx.arc(100, 100, 50, 0, 270, false);
+ ctx.fill();
+ });*/
+/*window.onload = function () {
+ var canvas = document.getElementById('canvasConstructor');
+ var ctx = canvas.getContext('2d');
+ ctx.fillRect(10, 10, 300, 100);
+ }*/
 $(document).ready(function (event, ui) {
+    $(".point").draggable({
+        start: function (event, ui) {
+            ui.helper.css({"z-index": 6});
+        },
+        revert: "invalid",
+        containment: "parent"
+    });
     $(".draggable").draggable({
         start: function (event, ui) {
-          ui.helper.css({"z-index": 6});
+            ui.helper.css({"z-index": 6});
         },
         revert: "invalid",
         helper: "clone",
         cursor: "move",
+    });
+    $(".marker").droppable({
+        accept: ".pointA, .pointB",
+        tolerance: "fit",
+        drop: function (event, ui) {
+            if (ui.draggable.is(".pointA") || ui.draggable.is(".pointB")) {
+                console.log("done");
+            }
+        }
     });
 
     $(".constructor").droppable({
@@ -29,7 +60,7 @@ $(document).ready(function (event, ui) {
                 mark_clone.removeClass("draggable new");
                 mark_clone.removeAttr("onmousedown id");
                 draggable_el_construct(mark_clone);
-            }else {
+            } else {
                 let parentOffsetLeft = $(".constructor").offset().left;
                 let parentOffsetTop = $(".constructor").offset().top;
                 let left = ui.helper.position().left - parentOffsetLeft;
@@ -39,6 +70,7 @@ $(document).ready(function (event, ui) {
                 mark_clone.css({'left': left, 'top': top, 'position': "absolute"});
                 mark_clone.removeClass("draggable new");
                 mark_clone.removeAttr("onmousedown id");
+                if(mark_clone.is(""))
                 draggable_el_construct(mark_clone);
             }
         },
@@ -48,7 +80,15 @@ $(document).ready(function (event, ui) {
         }
     });
 });
-$(window).resize(function() {console.log("resized")});
+function draggable_el_construct(item) {
+    item.draggable();
+    if (item.is("marker")) {
+        item.droppable();
+    }
+}
+$(window).resize(function () {
+    console.log("resized")
+});
 function active_mark_on_top(item) {
     let id = item.getAttribute("id");
     $(".dragged").css("z-index", 5);
---
+++ scripts/svg_drag.js
@@ -0,0 +1,97 @@
+let i = 0;
+var blabla = null;
+$(document).ready(function (event, ui) {
+    $(".draggable").draggable({
+        start: function (event, ui) {
+            ui.helper.css({"z-index": 6});
+            ui.helper.addClass("transport_svg");
+
+        },
+        revert: "invalid",
+        helper: "clone",
+        cursor: "move",
+    });
+
+    $(".constructor").droppable({
+        accept: ".draggable",
+        hoverClass: "highlight",
+        tolerance: "intersect",
+        activate: function (event, ui) {
+            $(this).find(".h3").text("Drag it HERE");
+            $(this).find(".h3").css("background-color", "#ddd");
+        },
+        drop: function (event, ui) {
+            let svg_offset = $("svg.constructor").offset()
+            let left = ui.helper.position().left - svg_offset.left;
+            let top = ui.helper.position().top - svg_offset.top;
+            if (ui.draggable.is(".draggable")) {
+                $("#svg_constructor").append(ui.draggable.clone().children().addClass("new"));
+                let new_obj = $(".new");
+                new_obj.attr({"id": new_obj.attr("id") + "_" + i++});
+                if (new_obj.attr("id").substring(0, 4) === "line") {
+                    new_obj.find(".pointA").attr({"cx": left + 11, "cy": top + 11, "onmousedown": "dragObject(this)"});
+                    new_obj.find(".pointB").attr({"cx": left + 89, "cy": top + 89, "onmousedown": "dragObject(this)"});
+                    new_obj.find("line").attr({"x1": left + 11, "y1": top + 11, "x2": left + 89, "y2": top + 89});
+
+                } else {
+                    new_obj.children().attr({"onmousedown": "dragObject(this)", "cx": left + 50, "cy": top + 50});
+                }
+                new_obj.removeClass("new");
+                if(new_obj.find("line").length === 0){
+                    new_obj.mouseenter(function () {
+                        blabla = this;
+                    });
+                }
+
+                new_obj.mouseup(function () {
+                    debugger;
+                })
+            }
+        },
+        deactivate: function (event, ui) {
+            $(this).find(".h3").text("Let's drag Marker");
+            $(this).find(".h3").css("background-color", "rgba(215, 215, 215, 0.5)");
+        }
+    });
+
+});
+
+function dragObject(item) {
+    const DIV = $(".constructor");
+    $(document).mousemove(function (event) {
+        let a_mouseX = event.pageX;
+        let a_mouseY = event.pageY;
+        let r_mouseX = a_mouseX - DIV.offset().left;
+        let r_mouseY = a_mouseY - DIV.offset().top;
+        if (r_mouseX < 11) {
+            r_mouseX = 11;
+        }
+        if (r_mouseX > $("svg.constructor").width()) {
+            r_mouseX = $("svg.constructor").width() - 11;
+        }
+        if (r_mouseY < 11) {
+            r_mouseY = 11;
+        }
+        if (r_mouseY > $("svg.constructor").height()) {
+            r_mouseY = $("svg.constructor").height() - 11
+        }
+        let point = $(item).attr("class");
+        if (point === "pointA") {
+            let line = $(item).next();
+            line.attr({"x1": r_mouseX, "y1": r_mouseY});
+        } else if (point === "pointB") {
+            let line = $(item).prev();
+            line.attr({"x2": r_mouseX, "y2": r_mouseY});
+        }
+        $(item).attr({"cx": r_mouseX, "cy": r_mouseY});/*
+        $(point.substring(0, 5) === "point").on("mouseup", function () {
+            hover_item_id = $(this).attr("id")
+        })*/
+
+    });
+    $(document).mouseup(function () {
+        $(document).off("mousemove");
+    });
+}
+
+// marius was here
